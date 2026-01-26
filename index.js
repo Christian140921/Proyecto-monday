0d5098f2d7b4b89b65422f6eaa5993b6930c5b3c
Cristian vargas <cristianvargas6322@gmail.com>
2026-01-26 13:11:49 -0500

    Se hacen cambios en los mensajes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getMondayData } = require('./monday.service');
+const { getMondayData } = require('./services/monday.service');
 const { saveJsonToFile } = require('./utils/file.util');
 const { savePlainText } = require('./utils/text.util');
 
@@ -10,9 +10,9 @@ async function main() {
     saveJsonToFile('data/monday-data.json', data);
     savePlainText('data/monday-data.txt', data);
 
-    console.log('✅ JSON y TXT generados correctamente');
+    console.log('JSON y TXT generados correctamente');
   } catch (error) {
-    console.error('❌ Error:', error.message);
+    console.error('Error:', error.message);
   }
 }
 
