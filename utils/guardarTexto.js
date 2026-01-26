f3436fca9b7f4b0f3db151e65f98338d499a56f4
Cristian vargas <cristianvargas6322@gmail.com>
2026-01-26 17:23:11 -0500

    URL agregadas

--- utils/guardarTexto.js
+++ utils/guardarTexto.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 function savePlainText(filePath, data) {
   const fullPath = path.resolve(filePath);
+  const boardId = '18396523890';
+  const company = 'cristianvargas6322s-team-company';
 
   let content = 'DATOS DE MONDAY\n\n';
 
@@ -11,6 +13,8 @@ function savePlainText(filePath, data) {
     for (const key in item) {
       content += `${key}: ${item[key]}\n`;
     }
+    const itemUrl = `https://${company}.monday.com/boards/${boardId}/pulses/${item.id}`;
+    content += `URL: ${itemUrl}\n`;
     content += '\n------------------\n\n';
   });
 
